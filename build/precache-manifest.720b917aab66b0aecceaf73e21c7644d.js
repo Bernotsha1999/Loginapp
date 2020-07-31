b52c5e9cd42b4dfba3f4406214fae2c713735cea
BERNOTSHA <bernotshasekar277@gmail.com>
2020-07-31 16:43:04 +0530

    initial-commit

---
+++ build/precache-manifest.720b917aab66b0aecceaf73e21c7644d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6072f0ec68b2fa8a7df597750af4147d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8557025124b1dcfe299e",
+    "url": "./static/css/main.9147e2bd.chunk.css"
+  },
+  {
+    "revision": "d6cdaa5e9877878683d7",
+    "url": "./static/js/2.b0e836da.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "./static/js/2.b0e836da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8557025124b1dcfe299e",
+    "url": "./static/js/main.71709a4b.chunk.js"
+  },
+  {
+    "revision": "c7488b0e57aa2f505f5e",
+    "url": "./static/js/runtime-main.800e563e.js"
+  }
+]);
